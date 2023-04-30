d1aa166edf81e7aa653df30b7a1509a29bbc9181
ashok kumar <spider.akm@gmail.com>
2023-04-30 09:55:09 +0530

    added login frontend and call the backend api

---
+++ views/login.js
@@ -0,0 +1,43 @@
+const email=document.getElementById("email")
+const password=document.getElementById("password")
+const login=document.getElementById("login")
+const loginError=document.getElementById("loginError")
+const loginSuccess=document.getElementById("loginSuccess")
+
+login.addEventListener("click",loginPage)
+
+async function loginPage(e){
+    try{
+            e.preventDefault()
+            const login_obj={
+                email:email.value,
+                password:password.value
+            }
+            const data=await axios.post("http://localhost:4000/user/signup",login_obj)
+            //If email or password are wrong
+            if(data.data.message){
+                const errorText=document.createTextNode(data.data.message)
+                loginError.appendChild(errorText)
+                loginError.style.color="red"
+            setTimeout(()=>{
+                loginError.removeChild(errorText)
+            },3000)
+            
+        }
+
+     //If email and password are valid
+    if(data.data.success){
+        const successText=document.createTextNode(data.data.success)
+        loginSuccess.appendChild(successText)
+        loginSuccess.style.color="green"
+        setTimeout(()=>{
+            loginSuccess.removeChild(successText)
+        },3000)
+
+
+    }
+}catch{
+        console.log("error in login page FE")
+    }
+}
+
